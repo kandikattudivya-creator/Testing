9c146366d63495ee274bfd895bfc4ef980da344a
root <root@ip-172-31-0-33.ap-south-1.compute.internal>
2025-11-13 13:18:16 +0000

    stashed changes

--- check.py
+++ check.py
@@ -1,5 +1,16 @@
+<<<<<<< Updated upstream
 Dev 2 made some changes
 # Ask for user input
 name = input("Enter your name: ")
+
 # Print a greeting
 print(f"Hello, {name}! Welcome to Python 😊")
+
+num1 = 5
+num2 = 3
+result = num1 + num2
+
+print(f"The sum of {num1} and {num2} is {result}.")
+
+
+>>>>>>> Stashed changes
